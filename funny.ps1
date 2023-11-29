4f24c700a79ce48481b620d71eb50063d256eaed
Josh <66529773+Joshuahuahua@users.noreply.github.com>
2023-11-30 00:27:09 +0800

    Update funny.ps1

--- funny.ps1
+++ funny.ps1
@@ -1,2 +1,3 @@
-Write-Host "ahoy"
-Read-Host
+iwr https://cdn.discordapp.com/attachments/297545069953548291/1179454975714267236/Discord_jHFRdtTPw0.jpg -OutFile alister.jpg
+set-itemproperty -path "HKCU:Control Panel\Desktop" -name WallPaper -value (Resolve-Path alister.jpg)
+1..100 | ForEach-Object { rundll32.exe user32.dll, UpdatePerUserSystemParameters }
