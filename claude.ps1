a68bae48a681db6bee64e19474e4c948aa02d44e
Joshua Hollander <joshua.hollander@diversus.com.au>
2024-03-13 14:53:27 +0800

    remove querystring

--- claude.ps1
+++ claude.ps1
@@ -1,3 +1,3 @@
-iwr "https://diversus.com.au/-/media/Images/People/Claude-de-Lucia.ashx?h=1466&iar=0&w=1002&hash=EC11DE5700A09F5FCE167B3AE0DDA745" -OutFile claude.jpg
+iwr "https://diversus.com.au/-/media/Images/People/Claude-de-Lucia.ashx" -OutFile claude.jpg
 set-itemproperty -path "HKCU:Control Panel\Desktop" -name WallPaper -value (Resolve-Path claude.jpg)
 1..100 | ForEach-Object { rundll32.exe user32.dll, UpdatePerUserSystemParameters }
